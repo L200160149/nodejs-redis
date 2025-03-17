0db532cf4c7ebb5963ea3cb324cbf5bd6e9ae703
suprilab <supri.adify@gmail.com>
2025-03-17 15:48:36 +0700

    UID/token rate limiter

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,9 @@
 import express, { Request, Response } from "express";
 
-import { limitIpMiddleware } from "./middleware/rate-limiter-middleware";
+import {
+  limitIdMiddleware,
+  limitIpMiddleware,
+} from "./middleware/rate-limiter-middleware";
 import dotenv from "dotenv";
 dotenv.config();
 
@@ -10,6 +13,23 @@ const PORT = process.env.PORT || 3000;
 app.get(
   "/limit-ip",
   limitIpMiddleware,
+  async (req: Request, res: Response): Promise<any> => {
+    try {
+      return res
+        .status(200)
+        .json({ message: "Request allowed", attempts: req.rateLimitCount });
+    } catch (error: any) {
+      console.error("error", error.message);
+      res.status(500).json({
+        message: "Internal server error",
+      });
+    }
+  }
+);
+
+app.get(
+  "/limit-uid",
+  limitIdMiddleware,
   async (req: Request, res: Response): Promise<any> => {
     try {
       return res
