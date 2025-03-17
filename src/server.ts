2b0d567d62046ccfa53048ec459b223f46b09acc
suprilab <supri.adify@gmail.com>
2025-03-17 16:11:52 +0700

    redis caching

--- src/server.ts
+++ src/server.ts
@@ -5,6 +5,8 @@ import {
   limitIpMiddleware,
 } from "./middleware/rate-limiter-middleware";
 import dotenv from "dotenv";
+import { redisClient } from "./applications/redis";
+import { prismaClient } from "./applications/database";
 dotenv.config();
 
 const app = express();
@@ -44,7 +46,48 @@ app.get(
   }
 );
 
+app.get("/caching", async (req: Request, res: Response): Promise<any> => {
+  const start = new Date().getTime();
+  const cacheKey = "aggregate_users_by_location";
+
+  try {
+    const cachedData = await redisClient.get(cacheKey);
+
+    if (cachedData) {
+      console.info("⏳ Caching from redis");
+      const end = new Date().getTime();
+
+      return res.status(200).json({
+        message: `success (cached) in: ${end - start}ms`,
+        data: JSON.parse(cachedData),
+      });
+    }
+
+    console.log("⏳ Fetching from MySQL...");
+
+    const result = await prismaClient.user.groupBy({
+      by: ["location"],
+      _count: { id: true },
+    });
+
+    // 10 minutes expired
+    await redisClient.setEx(cacheKey, 600, JSON.stringify(result));
+
+    const end = new Date().getTime();
+
+    res.status(200).json({
+      message: `success in: ${end - start}`,
+      start: new Date(start).getMilliseconds(),
+      end: new Date(end).getMilliseconds(),
+      data: result,
+    });
+  } catch (error: any) {
+    console.error("Error", error?.message);
+    res.status(500).json;
+    ({ message: "Internal Server Error" });
+  }
+});
+
 app.listen(PORT, () => {
-  // main();
   console.log(`App running on port: ${PORT}`);
 });
